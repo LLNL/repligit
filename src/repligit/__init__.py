99fa50d3a0ff06b1f6658a364fb0cb68bfe641c8
Caetano Melone <cmelone@llnl.gov>
2025-03-26 16:02:58 -0700

    Add async client
    closes #2
    
    I added an `asyncio` module under `src/repligit`. It has the same API as
    the sync client.
    
    import example:
    
    ```
    from repligit.asyncio import fetch_pack, ls_remote, send_pack
    ```
    
    Full example located at `examples/git_mirror_async.py`.
    
    I tried to share as much logic as possible between the sync and async
    clients, but I had to write some handlers to deal with streaming and
    decoding.
    
    The sync client is mostly unchanged. I've confirmed the funcionality
    works, but I'm open to feedback on how to improve the organization of
    the code and if any optimizations can be made to performance.
    
    Also:
    - enforces a minimum of python 3.10
    - sets aiohttp as an optional dependency (repligit can be installed with
      pip install repligit[aiohttp])

--- src/repligit/__init__.py
+++ src/repligit/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ["repligit"]
+from repligit.client import fetch_pack, ls_remote, send_pack
+
+__all__ = ["ls_remote", "fetch_pack", "send_pack"]
--- src/repligit/client.py
+++ src/repligit/client.py
@@ -1,30 +1,15 @@
-from typing import Generator, List, Union
+from typing import List
 
 import requests
 
-
-def decode_lines(lines: Generator[str]) -> Generator[str]:
-    """Decode server response iterator into usable lines."""
-    for line in lines:
-        line_length = int(line[:4], 16)
-        yield line[4:line_length]
-
-
-def encode_lines(lines: List[Union[bytes, str]]) -> bytes:
-    """Build byte string from given lines to send to server."""
-    result = []
-    for line in lines:
-        if type(line) is str:
-            line = line.encode("utf-8")
-
-        result.append(f"{len(line) + 5:04x}".encode())
-        result.append(line)
-        result.append(b"\n")
-
-    return b"".join(result)
+from repligit.parse import (
+    decode_lines,
+    generate_fetch_pack_request,
+    generate_send_pack_header,
+)
 
 
-def ls_remote(url, username=None, password=None):
+def ls_remote(url: str, username: str = None, password: str = None):
     """Get commit hash of remote master branch, return SHA-1 hex string or
     None if no remote commits.
     """
@@ -38,21 +23,10 @@ def ls_remote(url, username=None, password=None):
         resp.encoding = "utf-8"
 
     lines = decode_lines(resp.iter_lines(decode_unicode=True))
-
     service_line = next(lines)
     assert service_line == "# service=git-upload-pack"
 
     return dict(reversed(line.split()) for line in lines if line)
-
-
-def generate_fetch_pack_request(want: str, haves: List[str]) -> bytes:
-    """Generate a git-upload packfile request to retrieve a packfile from a
-    remote server based on a list of have and want commits.
-    """
-    want_cmds = encode_lines([f"want {want}".encode()])
-    have_cmds = encode_lines([f"have {sha}".encode() for sha in haves])
-
-    return want_cmds + b"0000" + have_cmds + encode_lines([b"done"])
 
 
 def fetch_pack(
@@ -74,7 +48,6 @@ def fetch_pack(
         stream=True,
         timeout=None,
     )
-
     resp.raise_for_status()
 
     line_length = int(resp.raw.read(4), 16)
@@ -86,10 +59,6 @@ def fetch_pack(
         return None
 
 
-def generate_send_pack_header(ref: str, from_sha: str, to_sha: str) -> bytes:
-    return encode_lines([f"{from_sha} {to_sha} {ref}\x00 report-status"]) + b"0000"
-
-
 def send_pack(
     url: str,
     ref: str,
@@ -99,11 +68,12 @@ def send_pack(
     username: str = None,
     password: str = None,
 ):
+    """Send a packfile to a remote server."""
     url = f"{url}/git-receive-pack"
     auth = (username, password) if username or password else None
 
     header = generate_send_pack_header(ref, from_sha, to_sha)
-    recieve_pack_request = header + packfile.read()
+    receive_pack_request = header + packfile.read()
 
     resp = requests.post(
         url,
@@ -112,11 +82,13 @@ def send_pack(
         },
         stream=True,
         auth=auth,
-        data=recieve_pack_request,
+        data=receive_pack_request,
     )
-
     resp.raise_for_status()
 
+    if resp.encoding is None:
+        resp.encoding = "utf-8"
+
     lines = decode_lines(resp.iter_lines(decode_unicode=True))
-    assert next(lines) == b"unpack ok"
-    assert next(lines) == f"ok {ref}".encode()
+    assert next(lines) == "unpack ok"
+    assert next(lines) == f"ok {ref}"
